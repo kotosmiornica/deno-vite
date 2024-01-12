7aa9aa1babd15d846b86963619c8a2f61d3e1705
kotosmiornica <145379435+kotosmiornica@users.noreply.github.com>
2024-01-12 14:26:20 +0100

    Initial commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let response: String;
+
+  async function sendRequest() {
+    const res = await fetch("/api", {
+      method: "GET",
+    });
+
+    return res.text();
+  }
+
+  async function getHome() {
+    response = await sendRequest();
+  }
+</script>
+
+<main>
+  {response}
+
+  <button on:click={getHome}>Send request</button>
+</main>
